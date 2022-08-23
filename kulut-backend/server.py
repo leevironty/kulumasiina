96776613a1ab40cb39ce5e3067c5f68bf74a4aab
Leevi Rönty <30956409+leevironty@users.noreply.github.com>
2022-08-23 22:48:16 +0300

    userdata to db works

--- kulut-backend/server.py
+++ kulut-backend/server.py
@@ -1,6 +1,7 @@
 import flask
 import psycopg
 import secrets
+import io
 
 conn_str = 'postgresql://leevi@localhost:5432/kulut'
 
@@ -12,13 +13,25 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024**2
 def handle_submit():
     data = flask.request.get_data()
     print(data)
-    try:
+    # try:
         # log_submit(flask.request.form)
-        record_submit(flask.request.form, flask.request.files)
-    except Exception as e:
-        print(e)
-        return 'pieleen meni'
+    save_submission(flask.request.form, flask.request.files)
+    # except Exception as e:
+        # print(e)
+        # return 'pieleen meni'
     return {'msg': 'ok'}
+
+@app.get('/api/file/<fname>')
+def serve_files(fname: str):
+    print(fname)
+    with psycopg.connect(conn_str) as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT receipt_file FROM submissions WHERE filename = %s', (fname,))
+            res = cur.fetchone()
+    if res is None:
+        flask.abort(404)
+    file_data = res[0]
+    return flask.send_file(io.BytesIO(file_data), download_name=fname)
 
 def log_submit(data: dict[str, str]):
     name = data['fullName']
@@ -45,30 +58,43 @@ EXPENSE_COMMAND = """INSERT INTO submissions (
     receipt_file,
     received_at
 ) values (
-    %s,%s, %s, %s, %s, %s, %s, now()
+    %s,%s, %s, true, %s, %s, %s, now()
+);
+"""
+
+ALLOWANCE_COMMAND = """INSERT INTO submissions (
+    name,
+    iban,
+    social_security,
+    description,
+    is_expense,
+    value,
+    kms,
+    received_at
+) values (
+    %s, %s, %s, %s, false, %s, %s, now()
 );
 """
 
 
-def record_submit(data: dict[str, str], files):
+def save_submission(data: dict[str, str], files):
     name = data['fullName']
     iban = data['iban']
     social_security = data.get('socialSecurity', None)
-    print(files)
-    print(data.keys())
+    # print(data.keys())
     exp_ids, allow_ids = parse_keys(data)
     extensions = ['pdf', 'jpg', 'jpeg', 'png']
-    while exp_ids:
-        i = exp_ids.pop()
-        description = data[f'expense-item-{i}-desc']
-        value = data[f'expense-item-{i}-value']
-        file = files[f'expense-item-{i}-receipt']
-        fname = file.filename
-        extension = fname.split('.')[-1].lower()
-        assert extension in extensions, 'Forbidden extension'
-        file_data = file.read()
-        new_fname = f'{secrets.token_hex(8)}.{extension}'
-        with psycopg.connect(conn_str) as conn:
+    with psycopg.connect(conn_str) as conn:
+        while exp_ids:
+            i = exp_ids.pop()
+            description = data[f'expense-item-{i}-desc']
+            value = data[f'expense-item-{i}-value']
+            file = files[f'expense-item-{i}-receipt']
+            fname = file.filename
+            extension = fname.split('.')[-1].lower()
+            assert extension in extensions, 'Forbidden extension'
+            file_data = file.read()
+            new_fname = f'{secrets.token_hex(8)}.{extension}'
             with conn.cursor() as cur:
                 cur.execute(
                     EXPENSE_COMMAND,
@@ -76,15 +102,30 @@ def record_submit(data: dict[str, str], files):
                         name,
                         iban,
                         description,
-                        True,
                         float(value),
                         new_fname,
                         file_data,
-                    ))
-                conn.commit()
-
-
-    
+                    )
+                )
+        
+        while allow_ids:
+            i = allow_ids.pop()
+            description = data[f'allowance-item-{i}-desc']
+            value = data[f'allowance-item-{i}-value']
+            kms = data[f'allowance-item-{i}-kms']
+            with conn.cursor() as cur:
+                cur.execute(
+                    ALLOWANCE_COMMAND,
+                    (
+                        name,
+                        iban,
+                        social_security,
+                        description,
+                        float(value),
+                        float(kms)
+                    )
+                )
+        conn.commit()
 
 
 
--- kulut-frontend/src/routes/AllowanceItem.svelte
+++ kulut-frontend/src/routes/AllowanceItem.svelte
@@ -7,6 +7,7 @@
   let kms: string = '';
   $: id = `allowance-item-${i.slice(0, -1)}`;
   $: key_kms = `${id}-kms`;
+  $: key_value = `${id}-value`;
   $: value = String(Number(kms) * per_km);
 </script>
 
@@ -15,5 +16,6 @@
   <svelte:fragment slot='valueInput'>
     <input bind:value={kms} placeholder='0' type='text' class='form-control' name={key_kms}>
     <span class='input-group-text'>km x {per_km} €/km = {value}€</span>
+    <input type='text' value={value} name={key_value} hidden>
   </svelte:fragment>
 </AbstractItem>
