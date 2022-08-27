a729628e99301cfcde91874600dbcb22f8b2b593
Leevi Rönty <30956409+leevironty@users.noreply.github.com>
2022-08-28 01:50:56 +0300

    feedback up upload status

--- kulumasiina_frontend/src/routes/+page.svelte
+++ kulumasiina_frontend/src/routes/+page.svelte
@@ -13,6 +13,9 @@
     value: string
   }
   let costItems: Array<costItem> = [];
+  let uploading = false;
+  let statusMsg = '';
+  let status: null | 'success' | 'danger' = null;
 
   $: total = costItems.reduce((v, {value}) => v + Number(value), 0);
   $: needSocialSecurity = costItems.some(({kind}) => kind === 'allowance');
@@ -35,8 +38,16 @@
     costItems.splice(i, 1);
     costItems = costItems;
   }
+  
+  const setMsgClearTimeout = (timeout: number = 5000) => {
+    setTimeout(() => {
+      status = null;
+      statusMsg = '';
+    }, timeout)
+  }
 
   const handleSubmit = (event: SubmitEvent) => {
+    uploading = true;
     const { currentTarget } = event;
     console.log(event);
     console.log(currentTarget);
@@ -46,12 +57,41 @@
       method: 'POST',
       body: data,
     })
-      .then(res => res.json())
-      .then(data => console.log(data))
+      // .then(res => res.json())
+      // .then(data => console.log(data))
+      .then(async response => {
+        const isJson = response.headers.get('content-type')?.includes('application/json');
+        const data = isJson ? await response.json() : null;
+
+        // check for error response
+        if (!response.ok) {
+            // get error message from body or default to response status
+            const error = (data && data.message) || response.status;
+            return Promise.reject(error);
+        }
+      })
+      .then(() => {
+        status = 'success';
+        statusMsg = 'Upload succeeded';
+        setMsgClearTimeout();
+      })
+      .catch((err) => {
+        console.log({err});
+        status = 'danger';
+        statusMsg = `Upload failed. Status ${err}`
+        setMsgClearTimeout();
+      })
+      .finally(() => {uploading = false;})
   }
 </script>
 
 <Header/>
+{#if status !== null}
+<div class={`alert alert-${status}`} role="alert">
+  {statusMsg}
+</div>
+{/if}
+
 <div class='row'>
   <form id='form' class='col-lg-8' on:submit|preventDefault={handleSubmit}>
     <Group key='fullName' label='Full name'/>
@@ -67,13 +107,19 @@
       <div class='col-9'>
         <div class='row m-0 g-0'>
           <div class='col-6'>
-            <button type='button' class='btn btn-primary form-control' on:click={newExpense}>+ new expense</button>
+            <button type='button' class='btn btn-primary form-control' on:click={newExpense}>add expense</button>
           </div>
           <div class='col-6'>
-            <button type='button' class='btn btn-primary form-control' on:click={newAllowance}>+ new mileage allowance</button>  
+            <button type='button' class='btn btn-primary form-control' on:click={newAllowance}>add mileage allowance</button>  
           </div>
         </div>
-        <button type='submit' class='btn btn-secondary form-control mt-1 mb-4'>Submit</button>
+        <button type='submit' class='btn btn-secondary form-control mt-1 mb-4'>
+          {#if (!uploading)}
+          Submit
+          {:else}
+          <div class="spinner-border spinner-border-sm" role="status"/>
+          {/if}
+        </button>
       </div>
     </div>
   </form>
